dd3bdd411e14eeee31d6dc22c27a729e0481150f
Sreya-883 <117372573+Sreya-883@users.noreply.github.com>
2024-07-31 23:29:12 +0530

    Create 73-set-matrix-zeroes.cpp

---
+++ 73-set-matrix-zeroes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rz=0,cz=0;
+        int m=matrix.size(),n=matrix[0].size();
+        for(int i=0;i<n;i++) if(matrix[0][i]==0){rz=1; break;}
+        for(int i=0;i<m;i++) if(matrix[i][0]==0){cz=1; break;}
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        if(rz==1)
+            for(int i=0;i<n;i++) matrix[0][i]=0;
+        if(cz==1)
+            for(int i=0;i<m;i++) matrix[i][0]=0;
+    }
+};
