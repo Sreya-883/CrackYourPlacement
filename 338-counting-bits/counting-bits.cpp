e3e5984c6327c2b8f88242be96d58ce9bccb77ed
Sreya-883 <117372573+Sreya-883@users.noreply.github.com>
2024-08-06 22:51:04 +0530

    Time: 2 ms (75.88%) | Memory: 9.3 MB (57.77%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> countBits(int n) {
         vector<int>ans(n+1,0);
         for(int i=1;i<=n;i++){
-            ans[i]=__builtin_popcount(i);
+            ans[i]=ans[i/2]+(i&1);
         }
         return ans;
     }
